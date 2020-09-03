c2ef3931aa2585c9212662d4d618fb43d878aeb0
iTecAI <matteovh@gmail.com>
2020-09-03 08:02:24 -0400

    sep model

---
+++ models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, create_model
+
+class ConnectionModel(BaseModel): # Connection model
+    fingerprint: str
+    publicKey: str
+
+class LoginModel(BaseModel): # Login model
+    fingerprint: str
+    username: str
+    hashword: str
