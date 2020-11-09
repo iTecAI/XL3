492c286462a21c5e051d3fbd02dc34835063ca5e
iTecAI <matteovh@gmail.com>
2020-11-09 08:31:49 -0500

    change cmd logging

--- api/chatbot.py
+++ api/chatbot.py
@@ -8,6 +8,13 @@ from api.pycritter import *
 from api.images import *
 from _runtime import server, CONFIG
 import logging
+logger = logging.getLogger("uvicorn.error")
+
+def cond(c,t,f):
+    if c:
+        return t
+    else:
+        return f
 
 def chatbot_interpret(content,fingerprint,campaign,map):
     raw_split = content.split(' ')
@@ -24,7 +31,8 @@ def chatbot_interpret(content,fingerprint,campaign,map):
                 args[current]['value'].append(a)
     args = [{'name':i['name'],'value':' '.join(i['value'])} for i in args]
     args[0]['value'] = args[0]['value'].split(' ')
-    print(cmd,args)
+
+    logger.info(f'User {fingerprint} sent command {cmd} with args {str(args)} to map {map} in campaign {campaign}')
 
     if cmd in ['r','roll']:
         roll_args = []
@@ -41,13 +49,13 @@ def chatbot_interpret(content,fingerprint,campaign,map):
             c = 0
             for arg in roll_args:
                 if 'd' in arg:
-                    roll_args[c] = '2d'+roll_args[c].split('d')[1][:len(roll_args[c].split('d')[1])-1]+'kh1'+roll_args[c].split('d')[1][len(roll_args[c].split('d')[1])-1]
+                    roll_args[c] = '2d'+roll_args[c].split('d')[1][:len(roll_args[c].split('d')[1])-1]+'kh1'+cond(roll_args[c].split('d')[1][len(roll_args[c].split('d')[1])-1] in ['+','-','*','/'], roll_args[c].split('d')[1][len(roll_args[c].split('d')[1])-1],'')
                 c+=1
         elif 'dis' in args[0]['value']:
             c = 0
             for arg in roll_args:
                 if 'd' in arg:
-                    roll_args[c] = '2d'+roll_args[c].split('d')[1][:len(roll_args[c].split('d')[1])-1]+'kl1'+roll_args[c].split('d')[1][len(roll_args[c].split('d')[1])-1]
+                    roll_args[c] = '2d'+roll_args[c].split('d')[1][:len(roll_args[c].split('d')[1])-1]+'kl1'+cond(roll_args[c].split('d')[1][len(roll_args[c].split('d')[1])-1] in ['+','-','*','/'], roll_args[c].split('d')[1][len(roll_args[c].split('d')[1])-1],'')
                 c+=1
         roll_args = ''.join(roll_args)
         return str(d20.roll(roll_args))
